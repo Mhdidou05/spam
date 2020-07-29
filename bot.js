9e8ce3792cba6f9c41a325034a183c9c31d04238
Mhdidou05 <67710431+Mhdidou05@users.noreply.github.com>
2020-07-29 21:17:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
-const myid = ['Mhdidou'];
+const myid = ['
+              458371482201096202'];
 const prefix = ['!'];
 const client = new Discord.Client();
 const client2 = new Discord.Client();
